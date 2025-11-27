d97a6658c3c515f0e1e0f5fd4f2b85c2eeaa4b19
Гураль Анатолій <tolik7327148@gmail.com>
2025-11-27 12:37:50 +0200

    Added comments

--- lists-PJ.py
+++ lists-PJ.py
@@ -1,20 +1,21 @@
+#funcs
 def sort(list):
     return sorted(list)
 def reverse(list):
     return list[::-1]
 def list_in_str(list):
     return ", ".join(list)
-
+#list generator
 amount = int(input("кількість елементів в списку: "))
 lists = [input("el: ") for i in range(amount)]
-
+#explanation
 print("____list-convertor____")
 print("1: сортуввання")
 print("2: задом наперед")
 print("3: об'єдняння")
-
+#user choise
 user = int(input("Введіть ваш вибір: "))
-
+#main code
 if user == 1:
     print(sort(lists))
 elif user == 2:
